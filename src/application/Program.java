b536ae5ccf527d4c3f5b6d6e9c46eefdcdf01d22
phmeyreles <principalphmeyreles3@gmail.com>
2021-10-18 17:49:01 -0300

    Contravariance

--- src/application/Program.java
+++ src/application/Program.java
@@ -8,11 +8,15 @@ public class Program {
 
 	public static void main(String[] args) {
 
-		List<Integer> intList = new ArrayList<Integer>();
-		intList.add(10);
-		intList.add(5);
-		List<? extends Number> list = intList;
-		Number x = list.get(0);
-		list.add(20); // compilation error
+		List<Object> myObjs = new ArrayList<Object>();
+		myObjs.add("Maria");
+		myObjs.add("Alex");
+
+		List<? super Number> myNums = myObjs;
+
+		myNums.add(10);
+		myNums.add(3.14);
+
+		Number x = myNums.get(0); // compilation error
 	}
 }
